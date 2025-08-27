e1337e82818fe87a224ef40bd7920ae2da2008ff
vicky-voin <alteneria127@gmail.com>
2025-08-27 14:27:14 -0400

    Minor cleanup

--- src/index.js
+++ src/index.js
@@ -7,50 +7,69 @@ import {SidebarView} from "./views/sidebarView"
 import { ProjectView } from "./views/projectView"
 import { TodoView } from "./views/todoView"
 
-const defaultProject = createDefaultProject();
-createDefaultTasks().forEach(todo => {
-    defaultProject.addToDo(todo);
-});
-
-const libraryStorageKey = "__ToDoApp__library"; 
-const myLibrary = new Library(libraryStorageKey, defaultProject);
-
-window.addEventListener('beforeunload', () => 
+class Data
 {
-    myLibrary.saveToStorage();
-});
+    #library;
 
-const sidebarRoot = document.querySelector(".sidebar-view-container");
-const sidebarView = new SidebarView(myLibrary, document);
-sidebarView.eventEmitter.on('projectSelected', (project) => 
-{
-    projectView.show(project, document);
-});
-sidebarView.eventEmitter.on('addNewProject', (projectTitle) => 
-{
-    const newProject = new Project(projectTitle, myLibrary);
-    sidebarView.refreshProjectList(document, myLibrary);
-});
-sidebarRoot.appendChild(sidebarView.domObject);
+    constructor()
+    {
+        const defaultProject = createDefaultProject();
+        createDefaultTasks().forEach(todo => {
+            defaultProject.addToDo(todo);
+        });
 
-const projectViewRoot = document.querySelector(".project-view-container");
-const projectView = new ProjectView(myLibrary.getProjects()[0], document);
-projectView.eventEmitter.on('todoSelected', (todo) => 
-{
-    todoViewRoot.hidden = false;
-    todoView.show(todo);
-});
-projectViewRoot.appendChild(projectView.domObject);
+        const libraryStorageKey = "__ToDoApp__library"; 
+        this.#library = new Library(libraryStorageKey, defaultProject);
+    }
 
-const todoViewRoot = document.querySelector(".todo-view-container");
-const todoView = new TodoView(document);
-todoView.eventEmitter.on('deleteTodo', (todo) => 
+    get library() { return this.#library; }
+}
+
+class MainView
 {
-    let project = myLibrary.getProjectWithId(todo.projectId);
-    project.deleteToDoWithId(todo.id);
-    todoView.clear();
-    todoViewRoot.hidden = true;
-    projectView.show(project, document);
-});
-todoViewRoot.hidden = true;
-todoViewRoot.appendChild(todoView.domObject);
+    constructor(library, document)
+    {
+        window.addEventListener('beforeunload', () => 
+        {
+            library.saveToStorage();
+        });
+
+        const sidebarRoot = document.querySelector(".sidebar-view-container");
+        const sidebarView = new SidebarView(library, document);
+        sidebarView.eventEmitter.on('projectSelected', (project) => 
+        {
+            projectView.show(project, document);
+        });
+        sidebarView.eventEmitter.on('addNewProject', (projectTitle) => 
+        {
+            const newProject = new Project(projectTitle, library);
+            sidebarView.refreshProjectList(document, library);
+        });
+        sidebarRoot.appendChild(sidebarView.domObject);
+
+        const projectViewRoot = document.querySelector(".project-view-container");
+        const projectView = new ProjectView(library.getProjects()[0], document);
+        projectView.eventEmitter.on('todoSelected', (todo) => 
+        {
+            todoViewRoot.hidden = false;
+            todoView.show(todo);
+        });
+        projectViewRoot.appendChild(projectView.domObject);
+
+        const todoViewRoot = document.querySelector(".todo-view-container");
+        const todoView = new TodoView(document);
+        todoView.eventEmitter.on('deleteTodo', (todo) => 
+        {
+            let project = myLibrary.getProjectWithId(todo.projectId);
+            project.deleteToDoWithId(todo.id);
+            todoView.clear();
+            todoViewRoot.hidden = true;
+            projectView.show(project, document);
+        });
+        todoViewRoot.hidden = true;
+        todoViewRoot.appendChild(todoView.domObject);
+    }
+}
+
+const dataStorage = new Data();
+const mainView = new MainView(dataStorage.library, document);
